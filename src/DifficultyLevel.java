b544d3cee6549d56482e3328837f35f4a3e9386c
Fred Morrison <frederick.morrison@outlook.com>
2024-12-12 17:12:12 -0700

    provide access to value and description for DifficultyLevel and make sure values are unique

--- src/DifficultyLevel.java
+++ src/DifficultyLevel.java
@@ -1,5 +1,22 @@
 public enum DifficultyLevel {
-    EASY,
-    MEDIUM,
-    HARD
+    EASY(0b0000, "Easy"),
+    MEDIUM(0b0001, "Medium"),
+    HARD(0b0010, "HARD"),
+    EVIL(0b0100, "EVIL");
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    private final int value;
+    private final String description;
+
+    DifficultyLevel(int value, String description) {
+        this.value = value;
+        this.description = description;
+    }
 }
